f05bccfaa6b27097552b49d79d875aa93f787287
pankaj-gitcode <pankajaditya75@gmail.com>
2025-03-13 19:07:04 +0530

    DB-bug fixed v1

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // ----- CONNECT MONGO -----
 const mongoConnect = async ()=>{
     try{
         await mongoose.connect(`${process.env.MONGO_URL}/qpayDB1`)
         .then(data=>console.log("DB Connected"))
-    }
-    catch(err){
+    }    catch(err){
         console.log("ERROR in Mongo-DB-Connection", err.message);
     }
 }
